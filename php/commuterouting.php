3109715616692b32501f50ec77eb744b78223b21
HansSkaarup <Hemd12@gmail.com>
2017-01-15 21:23:54 +0100

    1.1 Commuterouting implemented

---
+++ php/commuterouting.php
@@ -0,0 +1,60 @@
+<?php
+
+function escapeJsonString($value) { # list from www.json.org: (\b backspace, \f formfeed)
+  $escapers = array("\\", "/", "\"", "\n", "\r", "\t", "\x08", "\x0c");
+  $replacements = array("\\\\", "\\/", "\\\"", "\\n", "\\r", "\\t", "\\f", "\\b");
+  $result = str_replace($escapers, $replacements, $value);
+  return $result;
+}
+ 
+
+# Connect to PostgreSQL database
+$conn = pg_connect("dbname='Semester1' user='postgres' password='postgres' host='localhost'");
+if (!$conn) {
+    echo "Not connected : " . pg_error();
+    exit;
+}
+# If no input to adress, echo error message. Otherwise continue and set variable adress = input from GET function.
+if (empty($_GET['coordinates'])) {
+    echo "missing required paramater: <i>coordinates</i>";
+    exit;
+} else
+    $coordinates = $_GET['coordinates'];
+
+# Build SQL SELECT statement and return the geometry as a GeoJSON element in EPSG: 4326
+$sql = "select seq, cost, name, st_asgeojson(geom, 4326) as geojson, heading FROM pgr_fromAtoB('ways'," . $coordinates . ")";
+
+
+# Try query or error
+$rs = pg_query($conn, $sql);
+if (!$rs) {
+    echo "An SQL error occured.\n";
+    exit;
+}
+
+# Build GeoJSON
+$output    = '';
+$rowOutput = '';
+
+while ($row = pg_fetch_assoc($rs)) {
+    $rowOutput = (strlen($rowOutput) > 0 ? ',' : '') . '{"type": "Feature", "geometry": ' . $row['geojson'] . ', "properties": {';
+    $props = '';
+    $id    = '';
+    foreach ($row as $key => $val) {
+        if ($key != "geojson") {
+            $props .= (strlen($props) > 0 ? ',' : '') . '"' . $key . '":"' . escapeJsonString($val) . '"';
+        }
+        if ($key == "id") {
+            $id .= ',"id":"' . escapeJsonString($val) . '"';
+        }
+    }
+    
+    $rowOutput .= $props . '}';
+    $rowOutput .= $id;
+    $rowOutput .= '}';
+    $output .= $rowOutput;
+}
+
+$output = '{ "type": "FeatureCollection", "features": [ ' . $output . ' ]}';
+echo $output;
+?>
